859cc9cf26ef74029140770b212be8d6b9e6e6c8
akmalirfan <foxkid1994@gmail.com>
2019-05-25 17:33:29 +0800

    Add line to run using RetroArch

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -e
 rgbasm -o $1.o $1.asm
 rgblink -o $1.gb $1.o
 rgbfix -v -p 0 $1.gb
-/home/akmalirfan/Games/BGB/bgb.exe $1.gb
+# /home/akmalirfan/Games/BGB/bgb.exe $1.gb
+retroarch -L /usr/lib/libretro/gambatte_libretro.so $1.gb
